9d95d86a559b4c72e3bf78767a76c736e9b290d6
Tim Bailey-Jones <tim@bailey-jones.com>
2026-03-18 02:55:26 -0400

    add favicon - treble clef

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/css");
   eleventyConfig.addPassthroughCopy("src/js");
   eleventyConfig.addPassthroughCopy("src/images");
+  eleventyConfig.addPassthroughCopy({"src/favicon.svg": "favicon.svg"});
   eleventyConfig.addShortcode("year", () => String(new Date().getFullYear()));
 
   return {
